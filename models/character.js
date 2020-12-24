7015c916226930647da2db0563764b58e3d23ded
Parker Morgan <jpmaster13@msn.com>
2020-12-23 19:45:57 -0700

    character base model, needs updating

--- models/character.js
+++ models/character.js
@@ -1,65 +1,65 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+//const sequelize = require('../config/connection');
 
 class Character extends Model {}
 
 Character.init({
-    id: { 
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true, 
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id',
     },
-    user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id',
-        },
     date_created: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-        },
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
 
     //header
-    name: { type: DataTypes.STRING, allowNull: false, },
-    character_class: { type: dataTypes.STRING, },
-    character_level: { type: dataTypes.INTEGER, allowNull: false, },
-    background: { type: dataTypes.STRING, },
-    ancestry: { type: dataTypes.STRING, },
-    alignment: { type: dataTypes.STRING, },
+    name: { type: DataTypes.STRING, allowNull: false },
+    character_class: { type: dataTypes.STRING },
+    character_level: { type: dataTypes.INTEGER, allowNull: false },
+    background: { type: dataTypes.STRING },
+    ancestry: { type: dataTypes.STRING },
+    alignment: { type: dataTypes.STRING },
 
     //stats
-    strength: { type: dataTypes.INTEGER, allowNull: false, },
-    dexterity: { type: dataTypes.INTEGER, allowNull: false, },
-    constitution: { type: dataTypes.INTEGER, allowNull: false, },
-    intelligence: { type: dataTypes.INTEGER, allowNull: false, },
-    wisdom: { type: dataTypes.INTEGER, allowNull: false, },
-    charisma: { type: dataTypes.INTEGER, allowNull: false, },
+    strength: { type: dataTypes.INTEGER, allowNull: false },
+    dexterity: { type: dataTypes.INTEGER, allowNull: false },
+    constitution: { type: dataTypes.INTEGER, allowNull: false },
+    intelligence: { type: dataTypes.INTEGER, allowNull: false },
+    wisdom: { type: dataTypes.INTEGER, allowNull: false },
+    charisma: { type: dataTypes.INTEGER, allowNull: false },
 
     //defense
-    armor_class: { type: dataTypes.INTEGER, },
-    initiative: { type: dataTypes.INTEGER, },
-    speed: { type: dataTypes.INTEGER, },
+    armor_class: { type: dataTypes.INTEGER },
+    initiative: { type: dataTypes.INTEGER },
+    speed: { type: dataTypes.INTEGER },
 
     //health
-    max_hp: { type: dataTypes.INTEGER, },
-    current_hp: { type: dataTypes.INTEGER, },
-    temp_hp: { type: dataTypes.INTEGER, },
-    hit_dice_type: { type: dataTypes.INTEGER, },
-    hit_dice_total: { type: dataTypes.INTEGER, },
-    hit_dice_left: { type: dataTypes.INTEGER, },
-    death_save_successes: { type: dataTypes.INTEGER, },
-    death_save_failures: { type: dataTypes.INTEGER, },
-    
+    max_hp: { type: dataTypes.INTEGER },
+    current_hp: { type: dataTypes.INTEGER },
+    temp_hp: { type: dataTypes.INTEGER },
+    hit_dice_type: { type: dataTypes.INTEGER },
+    hit_dice_total: { type: dataTypes.INTEGER },
+    hit_dice_left: { type: dataTypes.INTEGER },
+    death_save_successes: { type: dataTypes.INTEGER },
+    death_save_failures: { type: dataTypes.INTEGER },
+
     //saving throws
-    str_save: { type: dataTypes.BOOLEAN, },
-    dex_save: { type: dataTypes.BOOLEAN, },
-    con_save: { type: dataTypes.BOOLEAN, },
-    int_save: { type: dataTypes.BOOLEAN, },
-    wis_save: { type: dataTypes.BOOLEAN, },
-    cha_save: { type: dataTypes.BOOLEAN, },
+    str_save: { type: dataTypes.BOOLEAN },
+    dex_save: { type: dataTypes.BOOLEAN },
+    con_save: { type: dataTypes.BOOLEAN },
+    int_save: { type: dataTypes.BOOLEAN },
+    wis_save: { type: dataTypes.BOOLEAN },
+    cha_save: { type: dataTypes.BOOLEAN },
 
     //skill card
     acrobatics: { type: dataTypes.BOOLEAN },
@@ -79,7 +79,7 @@ Character.init({
     religion: { type: dataTypes.BOOLEAN },
     sleight_of_hand: { type: dataTypes.BOOLEAN },
     stealth: { type: dataTypes.BOOLEAN },
-    survival:{ type: dataTypes.BOOLEAN },
+    survival: { type: dataTypes.BOOLEAN },
 
     //roleplay
     personality_traits: { type: dataTypes.STRING },
@@ -108,7 +108,7 @@ Character.init({
 
     //list of stuff
     features: { type: dataTypes.STRING },
-    }
+  },
 });
 
-module.exports = Character
+module.exports = Character;
