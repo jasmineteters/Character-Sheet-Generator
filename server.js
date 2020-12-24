396ab4b7352ec38e4503f865b5faf97147649a11
Alex Laursen <ajlaursen@aol.com>
2020-12-23 22:48:43 -0500

    created config folder/ connection file

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers.js');
 
-const sequelize = require('./confing/connection');
-const SequelizeStore = require('coonect-sessoin-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,7 +17,7 @@ const sess = {
   secret: 'the super secret awesome',
   cookie: {},
   resave: false,
-  saveUninitializd: true,
+  saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
   }),
