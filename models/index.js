f7411fd95d72bce0be095913d096143609a437c2
Parker Morgan <jpmaster13@msn.com>
2020-12-23 19:46:36 -0700

    index for user to character link

--- models/index.js
+++ models/index.js
@@ -1,12 +1,11 @@
-const Character = require("./character");
+const Character = require('./character');
 const User = require('./user');
 
 Character.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: 'user_id',
 });
 
 User.hasMany(Character, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE',
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
-
