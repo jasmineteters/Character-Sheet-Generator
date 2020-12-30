4910000303f247e7ff7656b8bfd291898d345ed4
Papaya Delight <jasmineteters2020@gmail.com>
2020-12-29 20:28:53 -0700

    added breakpoints to make the html responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      phone: '325px',
+
+      tablet: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
