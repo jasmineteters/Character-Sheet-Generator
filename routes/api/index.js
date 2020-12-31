b0a3cfc891723fa66d6adadf254cf13f7b5e4a4d
Alex Laursen <ajlaursen@aol.com>
2020-12-30 20:43:29 -0500

    cleaning up folder structure

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const characterRoutes = require('./characterRoutes');
-
-router.use('/users', userRoutes);
-router.use('/character', characterRoutes);
-
-module.exports = router;
