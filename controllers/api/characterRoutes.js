79a67329af4788ea494969439980b0fa501e20ed
Alex Laursen <ajlaursen@aol.com>
2021-01-06 20:42:30 -0500

    changed login buttons

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -3,37 +3,10 @@ const router = require('express').Router();
 const { Character } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// router.post('/', withAuth, async (req, res) => {
-//   console.log(req.body);
-//   if(req.params.id){
-//     try{
-//       Character.update(
-//         {...req.body},
-//         {where: req.params.id}
-//         )
-//         res.status(200+"updated");
-//       } catch (err) {
-//         res.status(400).json(err);
-//       }
-//     } else {
-//       try {
-//     const newCharacter = await Character.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
-
-//     res.status(200).json(newCharacter);
-//     res.redirect('/profile')
-//   } catch (err) {
-//     console.log("we done {explicit} up")
-//     console.log(req.body)
-//     res.status(400).json(err);
-//   }}
-// });
-
 router.post('/', withAuth, async (req, res) => {
-  const character = req.params.character
+  const character = req.body
   character.user_id = req.session.user_id
+  console.log(character);
   try {
     const newCharacter = await Character.create({
       character,
